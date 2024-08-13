0f58bf762b68797fdd0af3aa634c4d71a263b73d
Samuel <samuelleon2721@gmail.com>
2024-08-13 13:53:26 -0500

    routes

--- src/routes/petRoutes.ts
+++ src/routes/petRoutes.ts
@@ -6,9 +6,9 @@ import { authenticate,accessRole } from '../middlewares/authMiddlaware';
 const router = Router();
 
 router.post('/newPet',authenticate, createPet);
-router.get('/Pets',authenticate,accessRole(['empleado']),getAllPet);
-router.get('/userPets',authenticate,accessRole('user'),findPetsUser);
-router.put('/UpdatePet',authenticate,accessRole(['user','administrador']),updatePet);
-
+router.get('/Pets',authenticate,accessRole('administrador'),getAllPet)
+router.get('/Pet',authenticate,accessRole(['User']),findPetsUser)
+router.put('/UpdatePet',authenticate,accessRole(['User','administrador']),updatePet)
+router.get('/PetsUser',authenticate,accessRole(['administrador','empleado']),getAllPet)
 
 export default router;
