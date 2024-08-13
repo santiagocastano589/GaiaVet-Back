5276e7da5337925bfb28386ac1a3f17ec529c7d6
Santiago Castaño <santiagocastano589@gmail.com>
2024-08-13 09:55:15 -0500

    I upload update and delete products controllers x

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,12 +1,11 @@
 import { Router } from 'express';
-import { createProducto, getProducts, updateProducto, deleteProducto } from '../controllers/productoController';
-import { authenticate, accessRole } from '../middlewares/authMiddlaware';
+import {  updateUser, me,deleteAccount,createUser} from '../controllers/userController';
+import { authenticate,CustomRequest } from '../middlewares/authMiddlaware';
 
 const router = Router();
 
-router.post('/producto', authenticate, accessRole('administrador'), createProducto);
-router.get('/products', getProducts);
-router.put('/producto/:idProducto', authenticate, accessRole('administrador'), updateProducto);
-router.delete('/producto/:idProducto', authenticate, accessRole('administrador'), deleteProducto);
+router.put('/user', authenticate, updateUser); 
+router.get('/me',authenticate, me );
+router.put('/me/deleteAccount',authenticate,deleteAccount)
 
 export default router;
