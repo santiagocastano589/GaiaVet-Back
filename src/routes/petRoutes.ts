f8b6bc4af0e7ebed97102ac9e7346c3a92780595
Santiago Castaño <santiagocastano589@gmail.com>
2024-08-16 15:33:12 -0500

    I upload delete pet route

--- src/routes/petRoutes.ts
+++ src/routes/petRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { createPet ,getAllPet,findPetsUser,updatePet} from '../controllers/petControllers';
+import { createPet ,getAllPet,findPetsUser,updatePet, deletePet} from '../controllers/petControllers';
 import { authenticate,accessRole } from '../middlewares/authMiddlaware';
 
 
@@ -9,6 +9,7 @@ router.post('/newPet',authenticate, createPet);
 router.get('/Pets',authenticate,accessRole('administrador'),getAllPet)
 router.get('/Pet',authenticate,accessRole(['User']),findPetsUser)
 router.put('/UpdatePet',authenticate,accessRole(['User','administrador']),updatePet)
+router.put('/DeletePet',authenticate,accessRole(['User','administrador']),deletePet)
 router.get('/PetsUser',authenticate,accessRole(['administrador','empleado']),getAllPet)
 
 export default router;
