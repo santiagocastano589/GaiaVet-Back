791aff71b2220b6a89d13eb3ebe50adc3960cf42
Santiago Castaño <santiagocastano589@gmail.com>
2024-08-17 16:44:22 -0500

    I upload administrator route to see all registered users

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import {  updateUser, me,deleteAccount,createUser} from '../controllers/userController';
-import { authenticate,CustomRequest } from '../middlewares/authMiddlaware';
+import {  updateUser, me, deleteAccount, createUser, getAllUsers} from '../controllers/userController';
+import { authenticate,CustomRequest, accessRole } from '../middlewares/authMiddlaware';
 
 const router = Router();
 
 router.put('/user', authenticate, updateUser); 
+router.get('/users',authenticate,accessRole('administrador'),getAllUsers)
+
 router.get('/me',authenticate, me );
 router.put('/me/deleteAccount',authenticate,deleteAccount)
 router.post('/register',createUser)
