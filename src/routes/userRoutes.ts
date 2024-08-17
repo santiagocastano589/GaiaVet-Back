5abf66a771dd2051a9b6e2fd5a936af9c614c67d
Santiago Castaño <santiagocastano589@gmail.com>
2024-08-17 16:55:47 -0500

    I upload administrator route to search for a registered user

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import {  updateUser, me, deleteAccount, createUser, getAllUsers} from '../controllers/userController';
+import {  updateUser, me, deleteAccount, createUser, getAllUsers, findOneUser} from '../controllers/userController';
 import { authenticate,CustomRequest, accessRole } from '../middlewares/authMiddlaware';
 
 const router = Router();
 
 router.put('/user', authenticate, updateUser); 
 router.get('/users',authenticate,accessRole('administrador'),getAllUsers)
+router.get('/userFind/:cedula', authenticate, accessRole('administrador'), findOneUser);
 
 router.get('/me',authenticate, me );
 router.put('/me/deleteAccount',authenticate,deleteAccount)
