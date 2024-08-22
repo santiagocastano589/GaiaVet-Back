f4187d53ccce290e604a0586ac8fc85ca1215b45
Santiago Castaño <santiagocastano589@gmail.com>
2024-08-22 12:35:49 -0500

    Routes update

--- src/routes/petRoutes.ts
+++ src/routes/petRoutes.ts
@@ -9,7 +9,7 @@ router.post('/newPet',authenticate, createPet);
 router.get('/Pets',authenticate,accessRole('administrador'),getAllPet)
 router.get('/Pet',authenticate,accessRole(['User']),findPetsUser)
 router.put('/UpdatePet/:idMascota', authenticate, accessRole(['User', 'administrador']), updatePet);
-router.delete('/DeletePet',authenticate,accessRole(['User','administrador']),deletePet)
+router.put('/DeletePet',authenticate,accessRole(['User','administrador']),deletePet)
 router.get('/PetsUser',authenticate,accessRole(['administrador','empleado']),getAllPet)
 
 export default router;
