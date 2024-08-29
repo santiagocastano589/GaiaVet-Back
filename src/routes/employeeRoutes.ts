efaca96f69f9dfb8979e098c65f7d67228243e18
Santiago Castaño <santiagocastano589@gmail.com>
2024-08-29 14:46:15 -0500

    I upload route of the employees

--- src/routes/employeeRoutes.ts
+++ src/routes/employeeRoutes.ts
@@ -6,8 +6,8 @@ const router = Router();
 
 router.post('/empleado',authenticate,accessRole('administrador'), createEmployee);
 router.get('/employees',authenticate,accessRole('administrador'), getAllEmployees);
-router.get('/empleado/:cedulaEmpleado', getEmployeesById);
-router.put('/empleados/:cedulaEmpleado',updateEmployeed);
-router.delete('/empleados/:cedulaEmpleado', deleteEmployee);
+router.get('/empleado/:cedulaEmpleado',authenticate,accessRole('administrador'), getEmployeesById);
+router.put('/empleados/:cedulaEmpleado',authenticate,accessRole('administrador'),updateEmployeed);
+router.delete('/empleados/:cedulaEmpleado',authenticate,accessRole('administrador'), deleteEmployee);
 
 export default router;
