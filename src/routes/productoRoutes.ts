48837048c0b86bcde9490d9a471985862f32d785
SateHs <santiagoag365@gmail.com>
2024-09-05 15:23:33 -0500

    a

--- src/routes/productoRoutes.ts
+++ src/routes/productoRoutes.ts
@@ -10,7 +10,7 @@ router.put('/producto/:idProducto', authenticate, accessRole('administrador'), u
 router.delete('/producto/:idProducto', authenticate, accessRole('administrador'), deleteProducto);
 router.post('/create_preference', preferences_);
 
-router.post('/webhook', webhook);
+router.get('/webhook', webhook);
 
 
 export default router;
